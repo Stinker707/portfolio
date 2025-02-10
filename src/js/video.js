a060ac89915a952b719ca010ccf5821b5f85aa10
Oleg Ignatev <karambas707@gmail.com>
2025-02-10 14:18:22 +0300

    добавил настройки видео для safari

--- src/js/video.js
+++ src/js/video.js
@@ -2,6 +2,10 @@ import videoPath from '../video/background-fon.mp4';
 
 document.addEventListener('DOMContentLoaded', function() {
     $('#header').vide(videoPath, {
-        // опции
+        autoplay: 'muted',
+        loop: true,
+        muted: true,
+        playsinline: true,
+        controls: false
     });
 });

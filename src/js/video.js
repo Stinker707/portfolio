62b010f40a208857090c65f1165ee76cfe211178
Oleg Ignatev <karambas707@gmail.com>
2025-02-10 19:41:23 +0300

    настроил корректное отображение видео в safari

--- src/js/video.js
+++ src/js/video.js
@@ -8,9 +8,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var video = instance.getVideoObject();
     if (video) {
-        video.setAttribute('playsinline', 'true');
-        video.setAttribute('webkit-playsinline', 'true');
+        video.setAttribute('playsInline', 'true');
+        video.setAttribute('webkit-playsInline', 'true');
         video.muted = true;
         video.play().catch(err => console.warn('Autoplay failed:', err));
     }
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     $('#header').vide(videoPath, {
+//         autoplay: true,
+//         loop: true
+//     });
+    
+//     var video = document.querySelector('#header video');
+//     if (video) {
+//         video.setAttribute('playsinline', 'true');
+//         video.setAttribute('webkit-playsinline', 'true'); // Для iOS Safari
+//         video.muted = true;
+//         video.play().catch(err => console.warn('Autoplay failed:', err));
+//     }
+// });
